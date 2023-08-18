c1fc87a25aecb5b756aaf375816d441206765479
miroz devkota <mirozxy@gmail.com>
2023-08-18 13:44:27 +0545

    vercel database baby

--- prisma/migrations/20230818070756_todo/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "description" TEXT,
-    "completed" BOOLEAN DEFAULT false,
-    "user" TEXT NOT NULL
-);
