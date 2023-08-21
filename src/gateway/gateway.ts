d1976738805d0dc2ced16fb6a36fd45654e525af
miroz devkota <mirozxy@gmail.com>
2023-08-21 15:30:41 +0545

    socket

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,4 +1,4 @@
-import { OnModuleInit } from '@nestjs/common';
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
@@ -15,20 +15,44 @@ import { Server } from 'socket.io';
 export class MyGateWay implements OnModuleInit {
   @WebSocketServer()
   server: Server;
+  online = {};
+
+  allUsers = [];
+  onlineIds(myId) {
+    const filter = this.allUsers.filter((user) => user !== myId);
+    // this.online.push(filter);
+  }
 
   onModuleInit() {
     this.server.on('connection', (socket) => {
-      console.log('connected');
-      console.log('@@@@@@@', socket.id);
+      console.log('connected', socket.id);
+      this.allUsers.push(socket.id);
+      this.online[`${socket.id}_USER`] = socket.id;
+
+      socket.on('disconnect', () => {
+        console.log('disconnected user', socket.id);
+        const index = this.allUsers.indexOf(socket.id);
+        delete this.online[`${socket.id}_USER`];
+        if (index !== -1) {
+          this.allUsers.splice(index, 1);
+        }
+        console.log('users', this.allUsers, this.online);
+      });
+      // this.onlineIds(socket.id);
+
+      // this.server.emit('online', {
+      //   online: this.allUsers,
+      // });
+      console.log('users', this.allUsers, this.online);
     });
   }
 
   @SubscribeMessage('newMessage')
   onNewMessage(@MessageBody() body: any) {
     console.log('!!!!!!!!!!', body);
+
     this.server.emit('onMessage', {
-      msg: 'check content',
-      content: body,
+      online: this.online,
     });
   }
 }
