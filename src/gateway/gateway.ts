30571f353a418e10b99ae19bf7f6988631328615
miroz devkota <mirozxy@gmail.com>
2023-08-23 09:46:19 +0545

    push cors address

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ cors: true })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
